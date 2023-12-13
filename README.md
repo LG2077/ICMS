4ed5fa883047be7ccd6630ae3e539e5b3505a78c
giftlg <giftsankhulani@gmail.com>
2023-12-13 23:25:21 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+an android application for tracking and locating mobile phones using gps and gsm 
+by sending command to the phone which triggers a certain action 
+benefit of the application
+
+1 uses readily available resources and technology (gps,gsm and android devices)
+2 does not require active internet connection on both phone
+3 easy to use (the hard part is automated )
+4 support 98% of android devices 
+5 well optimised to manage android resources
---
+++ app/src/main/java/com/PEMPHO/ICMS/Splash.java
@@ -0,0 +1,33 @@
+package com.PEMPHO.ICMS;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this,MainActivity.class);
+                        startActivity(intent);
+                        finish();
+            }
+        },3000);
+
+
+
+    }
+}
